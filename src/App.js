6d41ea44f06bbfbdc48f1f968801336d6897bd04
shreyansh1910 <shreyansh.maurya.cd.mec20@itbhu.ac.in>
2023-05-07 05:41:11 +0530

    ver

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { useState } from 'react';
+
+//import CameraComponent from './camera';
+function App(){
+const [image, setImage] = useState(null);
+const [bgremove, setBgremove] = useState(null);
+
+
+const handleupload = () => {
+  const apikey = '1bNtgP9RRgGMg8EeQmdHBo7L'
+  const url = 'https://api.remove.bg/v1.0/removebg'
+  const formData = new FormData();
+  formData.append('image_file', image, image.name);
+  formData.append('size', 'auto');
+  fetch(url, {
+    method: 'POST', headers: { 'X-Api-Key': apikey }, body: formData
+  }
+  ).then(res => res.blob()).then(blob => {
+    const reader=new FileReader();
+    reader.onloadend=()=>{setBgremove(reader.result)}
+    reader.readAsDataURL(blob);
+
+
+}).catch(err => console.log(err));
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1 style={{textAlign:'center'}}>
+         Background remover
+      </h1>
+      <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+      <input type="file" onChange={ (e) => setImage(e.target.files[0])}></input>
+      <button onClick={handleupload}>upload image</button>
+      <div>{bgremove && <img width="500px" height="500px" src={bgremove} alt="bgremove" />}</div>
+      </div>
     </div>
+    
   );
 }
 
